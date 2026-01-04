e883602de537e7fedf852e2488f18a30bf4f7913
Alpesh Jadhav <djalpesh786@gmail.com>
2026-01-04 15:52:55 +0530

    first commit

---
+++ 01-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './app.css'
+
+const App = () => {
+  return (
+    <div id="container">
+      <h1 id="child1">Child 1</h1>
+      <h2 id="child2">Child 2</h2>
+    </div>
+  )
+}
+
+export default App
