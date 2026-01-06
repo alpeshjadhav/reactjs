eddd2636a0dda7e50a08f868e66dc8ab6b8b9dcb
Alpesh Jadhav <djalpesh786@gmail.com>
2026-01-06 20:13:47 +0530

    third commit

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className='parent'>
       <Card user='Abhishek Ghante' age={28} bio='Assistant Manager' img='https://images.unsplash.com/photo-1751097006268-ae4601fcb117?q=80&w=1518&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
-      <Card user='Kaustubh SHinde' age={21} bio='Senior Executive' img='https://images.unsplash.com/photo-1752801516481-cfb8c47ee9a3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
+      <Card user='Kaustubh Shinde' age={31} bio='Senior Executive' img='https://images.unsplash.com/photo-1752801516481-cfb8c47ee9a3?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
       <Card user='Alpesh Jadhav' age={29} bio='Manager' img='https://images.unsplash.com/photo-1751554933476-d029737d58b2?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
       <Card user='Pavan Borde' age={27} bio='Junier Executive' img='https://images.unsplash.com/photo-1751554933476-d029737d58b2?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
     </div>
