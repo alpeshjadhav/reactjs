33d498a4e88105aa10aff68e30c4fc38c3b82eca
Alpesh Jadhav <djalpesh786@gmail.com>
2026-01-17 20:06:32 +0530

    fifth commit

---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <h1 className={styles.logo}>Shersha</h1>
+      <button className={styles.btn}>Login</button>
+    </div>
+  )
+}
+
+export default Header
